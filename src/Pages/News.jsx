71081bf0175ca717dfa9c71fd3da447c681373e5
Ian McIntosh <imcintosh22@turing.edu>
2023-09-13 22:34:55 +0000

    added news part

--- src/Pages/News.jsx
+++ src/Pages/News.jsx
@@ -7,9 +7,9 @@ export const News = () => {
         <div className='news-container'>
             {news.map(({ id, title, datePublished, author, abstract, link, article }) => (
             <div key={id} className='news-article'>
-                <h1 className='article-title'>{title}</h1>
-                <small className='article-date'>Published:{datePublished}</small>
-                <small className='article-byline'>{author}</small>
+                <h2 className='article-title'>{title}</h2>
+                <p className='article-date'>Published:{datePublished}</p>
+                <aside className='article-byline'>by: {author}</aside>
                 <p className='article-abstract'>{abstract}</p>
             </div>
             ))}
