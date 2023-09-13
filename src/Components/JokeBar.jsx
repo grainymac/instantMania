b119419403f616994fba0a759be960a00795123c
Ian McIntosh <imcintosh22@turing.edu>
2023-09-13 19:45:04 +0000

    added joke data with random button

--- src/Components/JokeBar.jsx
+++ src/Components/JokeBar.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { jokes } from '../jokeData'
 
 export const JokeBar = () => {
+    const [randomJoke, setRandomJoke] = useState('')
+
+    const getRandomJoke = () => {
+        const randomIndex = Math.floor(Math.random() * jokes.length)
+        return jokes[randomIndex].joke
+    }
+
+    const handleNewJokeClick = () => {
+        setRandomJoke(getRandomJoke())
+    }
+
+    useEffect(() => {
+        setRandomJoke(getRandomJoke())
+    }, [])
+
     return (
         <>
             <div className='joke-container'>
-                {jokes[0].joke}
+                {randomJoke}
+                <button className='newJoke-button' onClick={handleNewJokeClick}>+</button>
             </div>
         </>
     )
--- src/jokeData.js
+++ src/jokeData.js
@@ -1,5 +1,38 @@
 export const jokes = [
     {
-        joke: 'Why couldnt the photographer see straight? He took too many shots.',
+        joke: "Why couldnt the photographer see straight? He took too many shots.",
+    },
+    {
+        joke: "When I was a kid if I was naughty my dad use to hit me with polaroid camera. To this day I can still have instant flashbacks.",
+    },
+    {
+        joke: "What do an infant and a Polaroid have in common? If you shake them too much, they don't develop properly.",
+    },
+    {
+        joke: "I just took a Polaroid of a breaking news event! More on this story as it develops",
+    },
+    {
+        joke: "I have Bi-Polaroid Disorder. All that comes out of me is negatives",
+    },
+    {
+        joke: "What do Eskimos get from sitting on the ice for too long? Polaroids",
+    },
+    {
+        joke: "Someone surprised me by jumping out and taking a Polaroid of me. Afterwards I was visibly shaken",
+    },
+    {
+        joke: "We take photographs as a return ticket to a moment otherwise gone.",
+    },
+    {
+        joke: "Just me, my camera, and I.",
+    },
+    {
+        joke: "Forever chasing good light.",
+    },
+    {
+        joke: "Sunny rays, and Polaroid days.",
+    },
+    {
+        joke: "Low quality pic, but high quality people.",
     },
 ]
