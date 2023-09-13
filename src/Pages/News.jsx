0858a232b13eb516e3e2fe70fbc7c396ce87e77a
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-09-13 07:20:59 -0500

    ading the news data

--- src/Pages/News.jsx
+++ src/Pages/News.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
+import { news } from '../data/newsData.js'
+
 export const News = () => {
     return (
-        <div>News</div>
+        <div className='news-container'>
+            <div className='news-article'>
+                <h1 className='article-title'></h1>
+                <small className='article-date'>Published:</small>
+                <small className='article-byline'></small>
+                <p className='article-abstract'></p>
+            </div>
+        </div>
     )
 }
---
+++ src/data/newsData.js
@@ -0,0 +1,10 @@
+export const news = [
+    {
+        title: "Welcome to Instant Mania!!",
+        datePublished: "13 September 2023",
+        author: "Ian McIntosh",
+        abstract: "Welcome to Instant Mania v1.0! I'm very excited to be working on this web application.  My love for web development and instant photography combines is just dream.",
+        link: "",
+        article: "",
+    }
+]
