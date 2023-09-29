5c7470fe0a75b202b287a3f9b063bdfd4acd03ab
Ian McIntosh <imcintosh22@turing.edu>
2023-09-29 22:19:04 +0000

    added two news articles

--- src/data/newsData.js
+++ src/data/newsData.js
@@ -17,4 +17,22 @@ export const news = [
         link: "",
         article: "I am on week two of building out this site.  It's been fun and little challenging and I'm trying to not get sidetracked by thinking of the upcoming obstacles I am going to be running into eventually, but I can't seem to help it.  I've had alot of days off from working on it from being sick from Covid, so I have had plenty of time to think about the obstacles.  One of my biggest concerns though is how to finish a complete database/catalog to all of polaroid's film framees.  Do I just include Polaroid film and film frames, or do I include ALL OF IT, like from before Polaroid went bankrupt, when The Impossible Project took over, when it came Polaroid Originals, and then to current?  Alot of the information on the film and film frame types are scattered all over the internet.  I mean, that's kind of the sole reason I am developing this site, to have a centeralized location/hub for this information.  The most logical solution to my worry of having a complete database, would be to involve the community.  Complete it as much as possible on my own, and with help from users and the community, build upon that.  It's not just for me.  It's for ya'll too.",
     },
+    {
+        id: 3,
+        title: "Web App Public Test!!",
+        datePublished: "28 September 2023",
+        author: "Ian McIntosh",
+        abstract: "I will be at Polacon this weekend hopefully to show some people my work on this web app so far!",
+        link: "",
+        article: "It has been about four weeks straight of me working on this site, and I am excited and nervous to show some people this weekend at Polacon to hopefully get some constructive feedback on somethings they would like to see or some things that I could improve on.  I will be deploying the app on Friday on doing some private test on the app to make sure it looks alright on mobiles devices.  The plan really is to just get it to the point where users can navigate and check it out.  Some features MAY be on he chopping bored, but I will elaborate more on what's staying on this site and whats to come.  I have alot of big plans for instantMania and I am very excited to be working on this and also very nervous.  I just want to do a good job and provide a great service for this community that I care so much about.",
+    },
+    {
+        id: 4,
+        title: "It's Deployed!!!!",
+        datePublished: "29 September 2023",
+        author: "Ian McIntosh",
+        abstract: "instantmania.vercel.app",
+        link: "",
+        article: "I just want to document that I deployed my application today getting it ready for this weekend to show off to a bunch of people to hopefully get some great feedback.  There are still some responsive design code I need to iron out, but I am pretty positive people will dig it.",
+    },
 ]
