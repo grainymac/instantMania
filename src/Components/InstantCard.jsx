5181698546e32621d0c22a60df9ec8deccd65b08
Ian McIntosh <imcintosh22@turing.edu>
2023-10-02 20:02:22 +0000

    i dont know

--- src/Components/InstantCard.jsx
+++ src/Components/InstantCard.jsx
@@ -1,58 +1,22 @@
 import React from 'react'
+import { polaroidFilmData } from '../data/polaroidFilmData'
 
-export const InstantCard = () => { 
+export const InstantCard = () => {
+    
+    const film = polaroidFilmData
     return (
         <div className="card-container">
-            <h2 className="film-title">B&W Film</h2>
+            <h2 className="title">Polaroid Film & Frames</h2>
+            {film.map(({ id, title, image }) => (
+                <div key={id} className='polaroid-card-container'>
+                    <img className='film-image' src={image} alt='polaroid' />
+                    <h4 className='film-title'>{title}</h4>
+                </div>
+            ))}
+            <h2 className="title">Instax Film & Frames</h2>
             <div className="film-card-container">
-                {/* <img className='film' src={bnw600} alt='bnw' />
-                <img className='film' src={bnwItype} alt='bnw itype' />
-                <img className='film' src={bnwsx70} alt='bnw sx70' /> */}
             </div>
 
-            <h2 className="film-title">Color Film</h2>
-            <div className="film-card-container">
-                {/* <img className='film' src={color600} alt='color 600' />
-                <img className='film' src={colorItype} alt='color itype' />
-                <img className='film' src={colorSx70} alt='color sx70' />
-                <img className='film' src={colorSpectra} alt='color spectra' />
-                <img className='film' src={roundColor600} alt='color round frame' />
-                <img className='film' src={colorFrames600} alt='color  frames' />
-                <img className='film' src={summerHaze600} alt='summer haze' />
-                <img className='film' src={summerFruits600} alt='summer fruits' />
-                <img className='film' src={memphis600} alt='memphis' />
-                <img className='film' src={rainbow600} alt='rainbow' />
-                <img className='film' src={mickey600} alt='mickey' />
-                <img className='film' src={skins600} alt='skins' />
-                <img className='film' src={poisonedParadise600} alt='poisoned paradise' />
-                <img className='film' src={lucky8600} alt='lucky 8' />
-                <img className='film' src={goldDust600} alt='gold dust' />
-                <img className='film' src={festiveRed600} alt='festive red' />
-                <img className='film' src={gold600} alt='gold frame' />
-                <img className='film' src={metalRed600} alt='metallic red frame' />
-                <img className='film' src={colorBlackframeItype} alt='color black frame itype' />
-                <img className='film' src={summerItype} alt='summer itype' />
-                <img className='film' src={summerBluesItype} alt='summer blues itype' />
-                <img className='film' src={colorWaveItype} alt='color wave itype' />
-                <img className='film' src={roseGoldItype} alt='rose gold itype' />
-                <img className='film' src={goldenMomentsItype} alt='golden moments itype' />
-                <img className='film' src={davidBowieItype} alt='david bowie itype' />
-                <img className='film' src={camoItype} alt='camo itype' />
-                <img className='film' src={daydreamItype} alt='daydream itype' />
-                <img className='film' src={noteItype} alt='note itype' />
-                <img className='film' src={timezeroSx70} alt='time zero sx70' />
-                <img className='film' src={gridSpectra} alt='grid spectra' />
-                <img className='film' src={colorBlackFrameGo} alt='color black frame go' /> */}
-            </div>
-
-            <h2 className="film-title">Duochrome Film</h2>
-            <div className="film-card-container">
-                {/* <img className='film' src={blackAndYellow} alt='black and yellow duochrome' />
-                <img className='film' src={blackAndGreen} alt='black and green duochrome' />
-                <img className='film' src={blackAndBlue} alt='black and blue duochrome' />
-                <img className='film' src={blackAndPink} alt='black and pink duochrome' />
-                <img className='film' src={reclaimedBlue} alt='black and green duochrome' /> */}
-            </div>
         </div>
     )
 } 
---
+++ src/data/polaroidFilmData.js
@@ -0,0 +1,132 @@
+import eleyKishimoto from '../polaroidFilm/bnwPolaroid/ely.jpg'
+import color600 from '../polaroidFilm/colorPolaroid/IMG_6103.jpg'
+import reclaimedBlue from '../polaroidFilm/duoPolaroid/IMG_6126.jpg'
+import metallicRedFrame from '../polaroidFilm/colorPolaroid/IMG_6095.jpg'
+import goldFrame from '../polaroidFilm/colorPolaroid/IMG_6098.jpg'
+import roundFrame from '../polaroidFilm/colorPolaroid/IMG_6102.jpg'
+import summerHaze from '../polaroidFilm/colorPolaroid/IMG_6128.jpg'
+import summerFruits from '../polaroidFilm/colorPolaroid/IMG_6131.jpg'
+import memphis from '../polaroidFilm/colorPolaroid/IMG_6133.jpg'
+import colorFrames from '../polaroidFilm/colorPolaroid/IMG_6138.jpg'
+import goldDust from '../polaroidFilm/colorPolaroid/IMG_6139.jpg'
+import festiveRed from '../polaroidFilm/colorPolaroid/IMG_6140.jpg'
+import skins from '../polaroidFilm/colorPolaroid/IMG_6178.jpg'
+import lucky8 from '../polaroidFilm/colorPolaroid/IMG_6179.jpg'
+import poisonedParadise from '../polaroidFilm/colorPolaroid/IMG_6187.jpg'
+import mickey from '../polaroidFilm/colorPolaroid/IMG_6188.jpg'
+import rainbow from '../polaroidFilm/colorPolaroid/IMG_6189.jpg'
+import bnw600 from '../polaroidFilm/bnwPolaroid/IMG_6096.jpg'
+import blackAndPink from '../polaroidFilm/duoPolaroid/IMG_6197.jpg'
+import blackAndBlue from '../polaroidFilm/duoPolaroid/IMG_6122.jpg'
+import blackAndGreen from '../polaroidFilm/duoPolaroid/IMG_6094.jpg'
+import blackAndYellow from '../polaroidFilm/duoPolaroid/IMG_6092 2.jpg'
+import blackAndRed from '../polaroidFilm/colorPolaroid/IMG_6181.jpg'
+import bnwColorFrame from '../polaroidFilm/colorPolaroid/IMG_6184 2.jpg'
+import deepSeaDive from '../polaroidFilm/colorPolaroid/IMG_6185.jpg'
+import hotPink from '../polaroidFilm/colorPolaroid/IMG_6345.jpg'
+import silverFrame from '../polaroidFilm/colorPolaroid/IMG_6350.jpg'
+import cyanMono from '../polaroidFilm/colorPolaroid/IMG_6353.jpg'
+import bnwHardColor from '../polaroidFilm/bnwPolaroid/IMG_6352.jpg'
+import iceCream from '../polaroidFilm/colorPolaroid/IMG_6195.jpg'
+import skateistan from '../polaroidFilm/bnwPolaroid/IMG_6356.jpg'
+import blackAndOrange from '../polaroidFilm/duoPolaroid/IMG_6180.jpg'
+import thirdManRecords from '../polaroidFilm/duoPolaroid/IMG_6357.jpg'
+import barbie from '../Polaroid/film/IMG_6401.jpg'
+import colorGo from '../Go/film/IMG_6121.jpg'
+import blackFrameGo from '../Go/film/IMG_6129.jpg'
+import iTypeColor from '../iType/film/IMG_6100.jpg'
+import bnwIType from '../iType/film/IMG_6119.jpg'
+import palace from '../iType/film/IMG_6120.jpg'
+import roseGold from '../iType/film/IMG_6123.jpg'
+import summerBlues from '../iType/film/IMG_6127.jpg'
+import goldenMoments from '../iType/film/IMG_6130.jpg'
+import colorWave from '../iType/film/IMG_6134.jpg'
+import colorBlackFrame from '../iType/film/IMG_6135.jpg'
+import summer from '../iType/film/IMG_6136.jpg'
+import davidBowie from '../iType/film/IMG_6177.jpg'
+import camo from '../iType/film/IMG_6183.jpg'
+import daydream from '../iType/film/IMG_6191.jpg'
+import noteThis from '../iType/film/IMG_6192.WEBP'
+import mandalorian from '../iType/film/IMG_6394.jpg'
+import spectrum from '../iType/film/IMG_6395.jpg'
+import strangerThings from '../iType/film/IMG_6397.jpg'
+import metallicSpectrum from '../iType/film/IMG_6399.jpg'
+import spectraColor from '../Spectra/film/IMG_6124.jpg'
+import grid from '../Spectra/film/IMG_6190.jpg'
+import spectraBnw from '../Spectra/film/IMG_6400.jpg'
+import sx70bnw from '../Sx70/film/IMG_6125.jpg'
+import timeZero from '../Sx70/film/IMG_6132.jpg'
+import sx70color from '../Sx70/film/IMG_6137.jpg'
+
+export const polaroidFilmData = [
+    {
+        title: 'B&W 600 Film',
+        image: bnw600,
+        link: '',
+    },
+    {
+        title: 'Eley Kishimoto Edition',
+        image: eleyKishimoto,
+        link: '',
+    },
+    {
+        title: 'Color 600 Film',
+        image: color600,
+        link: '',
+    },
+    {
+        title: 'Reclaimed Blue',
+        image: reclaimedBlue,
+        link: '',
+    },
+    {
+        title: 'Metallic Red Frame Edition',
+        image: metallicRedFrame,
+        link: '',
+    },
+    {
+        title: 'Round Film',
+        image: roundFrame,
+        link: '',
+    },
+    {
+        title: 'Gold Frame Edition',
+        image: goldFrame,
+        link: '',
+    },
+    {
+        title: 'Summer Haze Edition',
+        image: summerHaze,
+        link: '',
+    },
+    {
+        title: '600 B&W Film',
+        image: bnw600,
+        link: '',
+    },
+    {
+        title: '600 B&W Film',
+        image: bnw600,
+        link: '',
+    },
+    {
+        title: '600 B&W Film',
+        image: bnw600,
+        link: '',
+    },
+    {
+        title: '600 B&W Film',
+        image: bnw600,
+        link: '',
+    },
+    {
+        title: '600 B&W Film',
+        image: bnw600,
+        link: '',
+    },
+    {
+        title: '600 B&W Film',
+        image: bnw600,
+        link: '',
+    },
+]
