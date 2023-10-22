1a0b27859873ecf966b46fd9650d3df5757bab7a
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-10-22 15:39:29 -0500

    added drop down menu to instant film

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -9,46 +9,54 @@ export const NavBar = () => {
     const [query, setQuery] = useState('')
 
     const filteredResults = polaroidFilmData.filter((item) =>
-    item.title.toLowerCase().includes(query.toLowerCase())
-  );
+        item.title.toLowerCase().includes(query.toLowerCase())
+    );
 
-  const limitedResults = filteredResults.slice(0, 5)
+    const limitedResults = filteredResults.slice(0, 5)
 
-  const resetSearchBar = () => {
-    setQuery('')
-  }
+    const resetSearchBar = () => {
+        setQuery('')
+    }
 
     return (
         <>
-        <div className='navbar-container'>
-            <div className='logo-container'>
-                <NavLink to='/' className='nav-links'><h1>instantMania</h1></NavLink>
-            </div>
+            <div className='navbar-container'>
+                <div className='logo-container'>
+                    <NavLink to='/' className='nav-links'><h1>instantMania</h1></NavLink>
+                </div>
 
-            <div className='search-container'>
-                <input className='searchbar' placeholder='Search for Polaroid Film & Frames' type='text' onChange={e => setQuery(e.target.value)} value={query}/>
-                {query && (
-                    <div className='result-container'>
-                        <ul className='result-list'>
-                        {limitedResults.map((result, index) => (
-                            <li className='result' key={index}>
-                                <Link onClick={resetSearchBar} className='result-link' to={result.link}>{result.title}</Link>
-                            </li>
-                        ))}
-                        </ul>
+                <div className='search-container'>
+                    <input className='searchbar' placeholder='Search for Polaroid Film & Frames' type='text' onChange={e => setQuery(e.target.value)} value={query} />
+                    {query && (
+                        <div className='result-container'>
+                            <ul className='result-list'>
+                                {limitedResults.map((result, index) => (
+                                    <li className='result' key={index}>
+                                        <Link onClick={resetSearchBar} className='result-link' to={result.link}>{result.title}</Link>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    )}
+                </div>
+
+                <nav className='links-container'>
+                    <NavLink to='/' className='nav-links'>Home</NavLink>
+                    <NavLink to='/news' className='nav-links'>News</NavLink>
+                    <div className='nav-dropdown'>
+                        <NavLink to='/instant' className='nav-links'>Instant Film</NavLink>
+                            <div className='dropdown-content'>
+                                <NavLink to='/instant' className='dropdown-item'>All Instant Film</NavLink>
+                                <NavLink to='/polaroid' className='dropdown-item'>600</NavLink>
+                                <NavLink to='/itype' className='dropdown-item'>i-Type</NavLink>
+                                <NavLink to='/go' className='dropdown-item'>Go</NavLink>
+                                <NavLink to='/sx' className='dropdown-item'>SX-70</NavLink>
+                                <NavLink to='/spectra' className='dropdown-item'>Spectra</NavLink>
+                            </div>
                     </div>
-                )}
+                    <NavLink to='/galleries' className='nav-links'>Galleries</NavLink>
+                </nav>
             </div>
-
-            <div className='links-container'>
-                <ul className='links'>
-                    <li><NavLink to='/' className='nav-links'>Home</NavLink></li>
-                    <li><NavLink to='/news' className='nav-links'>News</NavLink></li>
-                    <li><NavLink to='/instant' className='nav-links'>Instant Film</NavLink></li>
-                    <li><NavLink to='/galleries' className='nav-links'>Galleries</NavLink></li>
-                </ul>
-            </div>
-        </div>
         </>
     )
 }
