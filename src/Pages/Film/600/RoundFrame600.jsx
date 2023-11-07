707cf2d7bfeb978290020ccdf9cdf31bf5991854
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-11-07 07:17:13 -0600

    roundd frame

--- src/Pages/Film/600/RoundFrame600.jsx
+++ src/Pages/Film/600/RoundFrame600.jsx
@@ -12,6 +12,9 @@ export const RoundFrame600 = () => {
                 <aside className='text-container'>
                     <p>
                     Comes with a pack of eight instant photos featuring circular frames, allowing your creativity to shine. Originally introduced in 2014 and still in production today.
+                    <br />
+                    <br />
+                    Using a circular frame while taking the shot forces you to rethink your composition and focus on capturing images that fit this specific shape. I encouraged myself to get up close to my subjects, resulting in tight and intimate close-up shots. Perfect frame if alot of your work are portraitures! 
                     </p>
                     <ul>
                         <b>Specs:</b>
