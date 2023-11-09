317ff5c0aa818584002fc73e8a08c23d482d08c7
Ian McIntosh <imcintosh22@turing.edu>
2023-11-09 20:48:52 +0000

    added film

--- src/Pages/Film/i-Type/BnwItype.jsx
+++ src/Pages/Film/i-Type/BnwItype.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../styles.css'
 import film from '../../../iType/film/IMG_6119.jpg'
+import { Link } from 'react-router-dom'
 
 export const BnwItype = () => {
 
@@ -16,12 +17,22 @@ export const BnwItype = () => {
                     <br />
                     Instant photography has made a comeback in recent years, and this modern film meets the needs of those who use contemporary instant cameras. Its dramatic shadow and soft highlight capabilities allow for creative and visually appealing shots. Additionally, the absence of a battery makes it more environmentally friendly, and its compatibility with various Polaroid camera models ensures flexibility for photographers. So, if you're looking for an affordable and hassle-free way to capture instant memories in black and white, this film could be an excellent choice for you.
                     </p>
+                    <ul>
+                        <b>Specs:</b>
+                        <li>Pack of eight instant black&white photos with white frames.</li>
+                        <li>ASA: 640</li>
+                        <li>Format: 3.5 x 4.2 in (8.8 x 10.7 cm).</li>
+                        <li>Image area: 3.1 x 3.1 in (7.9 x 7.9 cm).</li>
+                        <li>Images begin to emerge after 1 minutes.  Full development takes 5-10 minutes.</li>
+                    </ul>
                     <small>
                         <b>Film Type:</b> Black & White
                     </small>
                     <small>
-                        <b>Compatible Cameras:</b> i-Type cameras
+                    <b>Compatible Cameras:</b>Polaroid Now, Polaroid Now+, Polaroid Lab, OneStep 2, OneStep+, Polaroid i-2
                     </small>
+                    <Link to='/instant'>← back to all film & frames</Link>
+
                 </aside>
             </div>
         </div>
--- src/Pages/Film/i-Type/Camo.jsx
+++ src/Pages/Film/i-Type/Camo.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../styles.css'
 import film from '../../../iType/film/IMG_6183.jpg'
+import { Link } from 'react-router-dom'
 
 export const Camo = () => {
 
