b319281d416cbf30cc741065278adf48e31eb970
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-12-28 20:37:53 -0600

    dev page

---
+++ src/Pages/Development/Development.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Development.css'
+
+export const Development = () => {
+    return (
+        <>
+            <main className='dev-container'>
+            <h1 className='dev-title'>instantMania was developed using:</h1>
+            <div className='frontend-container'>
+                <label>Frontend</label>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>React</li>
+                </ul>
+            </div>
+            </main>
+        </>
+    )
+}
